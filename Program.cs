ef5b5721f042eaece7938995a6f051fad3c430ee
Maria Toledo <Nowaxial@users.noreply.github.com>
2025-03-20 11:33:03 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace firstNamelastName;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome! What's your name? ");
+        Console.Write("Type your first name: ");
+
+        
+        string myFirstName = Console.ReadLine()!;
+
+        Console.WriteLine(" Type your last name: ");
+        string myLastName = Console.ReadLine()!;
+
+        Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+        Console.ReadLine();
+
+
+    }
+}
